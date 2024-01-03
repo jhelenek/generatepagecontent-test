c3a59309e46213a9b2c4771e92b195c6b5ee9414
Joseph Ballschneider <jballschneider@yext.com>
2024-01-03 16:29:23 -0500

    clean up serve func

--- src/functions/http/preview/[templateId]/entity/[entityId].ts
+++ src/functions/http/preview/[templateId]/entity/[entityId].ts
@@ -1,8 +1,8 @@
 import { SitesHttpRequest, SitesHttpResponse } from "@yext/pages/*";
 import fetch from 'node-fetch';
 
-// API key for an app with SitesAPI read permissions
 const API_DOMAIN = 'https://api.yextapis.com';
+// API key for an app with SitesAPI read permissions
 const API_KEY = '1180012a82bbd5acd01414744612c3df';
 const V_PARAM = '20240103';
 
@@ -11,25 +11,25 @@ export default async function generatePreview(
 ): Promise<SitesHttpResponse> {
   const { pathParams, queryParams, site } = request;
 
-  // Get parametes for SitesAPI calls
+  // Get parameters for SitesAPI calls
   const templateId = pathParams['templateId'];
   const entityId = pathParams['entityId'];
+  const siteId = site.siteId;
+  const deploymentId = site.deployId;
 
   // Retrieve entity document
-  const entityResponse = await fetch(`${API_DOMAIN}/v2/accounts/me/sites/${site.siteId}/fetchentitydocument?entityId=${entityId}&templateId=${templateId}&deploymentId=${site.deployId}&api_key=${API_KEY}&v=${V_PARAM}`);
+  const entityResponse = await fetch(`${API_DOMAIN}/v2/accounts/me/sites/${siteId}/fetchentitydocument?entityId=${entityId}&templateId=${templateId}&deploymentId=${deploymentId}&api_key=${API_KEY}&v=${V_PARAM}`);
   const entityJson = await entityResponse.json();
   const entityData = entityJson.response.document;
 
   // Generate page content using fetched document
-  const generateResponse = await fetch(`${API_DOMAIN}/v2/accounts/me/sites/${site.siteId}/generatepagecontent?templateId=${templateId}&deploymentId=${site.deployId}&api_key=${API_KEY}&v=${V_PARAM}`, 
+  const generateResponse = await fetch(`${API_DOMAIN}/v2/accounts/me/sites/${siteId}/generatepagecontent?templateId=${templateId}&deploymentId=${deploymentId}&api_key=${API_KEY}&v=${V_PARAM}`, 
     {method: 'POST', body: JSON.stringify(entityData), headers: {'Content-Type': 'application/json'}});
   const generateJson = await generateResponse.json();
 
   // Fix relativePrefixToRoot for serverless function path.
   const html = generateJson.response.content as string;
   const htmlWithCorrectPath = html.replaceAll(/(\.\.\/)+/, '../../../');
-
-  console.log("done")
 
   return {
     body: htmlWithCorrectPath,
