27d293c1f842e3515ba2d077177f53ea2b3fb239
jhelenek <jhelenek@yext.com>
2024-01-22 15:36:27 +0000

    adding logging

--- src/functions/http/preview/[templateId]/entity/[entityId]/suggestion/[suggestionId].ts
+++ src/functions/http/preview/[templateId]/entity/[entityId]/suggestion/[suggestionId].ts
@@ -1,14 +1,19 @@
 import { SitesHttpRequest, SitesHttpResponse } from "@yext/pages/*";
 import fetch from 'node-fetch';
 
+console.log("In suggestion.ts");
+
 const API_DOMAIN = 'https://api.yextapis.com';
 // API key for an app with SitesAPI read permissions
 const API_KEY = '2ddc800c7a002cb2b6f80a2e6c1a7181';
 const V_PARAM = '20240103';
 
+console.log("Consts initialized");
+
 export default async function generatePreview(
   request: SitesHttpRequest
 ): Promise<SitesHttpResponse> {
+  console.log("Beginning of generatePreview");
   const { pathParams, queryParams, site } = request;
 
   // Get parameters for SitesAPI calls
@@ -18,16 +23,18 @@ export default async function generatePreview(
   const siteId = site.siteId;
   const deploymentId = site.deployId;
 
+  console.log("Before retrieving entity documents");
   // Retrieve entity document
   const entityResponse = await fetch(`${API_DOMAIN}/v2/accounts/me/sites/${siteId}/fetchentitydocument?entityId=${entityId}&templateId=${templateId}&deploymentId=${deploymentId}&editIds=${suggestionId}&api_key=${API_KEY}&v=${V_PARAM}`);
   const entityJson = await entityResponse.json();
   const entityData = entityJson.response.document;
-
+  console.log("After retrieving entity documents");
   // Generate page content using fetched document
+  console.log("Before generating response");
   const generateResponse = await fetch(`${API_DOMAIN}/v2/accounts/me/sites/${siteId}/generatepagecontent?templateId=${templateId}&deploymentId=${deploymentId}&api_key=${API_KEY}&v=${V_PARAM}`, 
     {method: 'POST', body: JSON.stringify(entityData), headers: {'Content-Type': 'application/json'}});
   const generateJson = await generateResponse.json();
-
+  console.log("After generating response");
   // Fix relativePrefixToRoot for serverless function path.
   const html = generateJson.response.content as string;
   const htmlWithCorrectPath = html.replaceAll(/(\.\.\/)+/g, '../../../');
